77c59470fdaf35ad9f1f1b9744658eb2ba2cf637
Sourabh Kumar <createownpath111@gmail.com>
2023-03-17 16:03:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function calculateMinCost() {
   //your code here
+  let inputData = document.querySelector("#rope-lengths").value;
+  var inputArr = inputData.split(",");
+  for(let i=0;i<inputArr.length;i++)
+	 inputArr[i]=Number(inputArr[i]);
   
-  
-  
+  var cost = 0;
+  inputArr.sort(function(a,b){return a-b});	
+  while(inputArr.length>1){
+	  let element = inputArr[0]+inputArr[1];
+	  cost += element;
+	  inputArr.splice(0,2);
+	  inputArr.push(element);
+	  inputArr.sort(function(a,b){return a-b});	
+  }	
+	document.querySelector("#result").textContent = cost;
 }  
